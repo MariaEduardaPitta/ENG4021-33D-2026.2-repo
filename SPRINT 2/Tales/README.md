7fd6bc0fa4b25ab68653b268732a5d4ad30778aa
talesnr1 <talesrj29@gmail.com>
2026-09-03 20:37:09 -0300

    Add radiacao function for square root calculation

--- SPRINT 2/Tales/README.md
+++
@@ -1,2 +0,0 @@
-# Tales
-Arquivos e entregas referentes às tarefas da Sprint 2.
---
+++ SPRINT 2/Tales/calculadora radiação.py
@@ -0,0 +1,5 @@
+def radiacao(x):
+    if x<0:
+      return 'raiz quadrada inexistente'
+    else:
+      return x**0.5
