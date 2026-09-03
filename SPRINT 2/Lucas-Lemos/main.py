e9521690ef568e61c2001a901874a9af55a1a046
Lucas Fender <lucas08pfaender@gmail.com>
2026-09-03 13:34:48 -0300

    Add integer division function with zero check

--- SPRINT 2/Lucas-Lemos/main.py
+++ SPRINT 2/Lucas-Lemos/main.py
@@ -1,5 +1,9 @@
-def calcula_divisao(x,y):
-  return x/y
+def calcula_divisao_inteira(x,y):
+  if y == 0:
+        return "Não é possível dividir por zero"
+  return x//y
 
 def calcula_resto (x,y):
+  if y == 0:
+        return "Não é possível dividir por zero"
   return x % y
